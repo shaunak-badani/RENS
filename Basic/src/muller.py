cb1c115f6dc939073fc74875733700225c4ba5e3
Shaunak <shaunak.badani@research.iiit.ac.in>
2021-12-14 12:38:53 +0530

    Changed force function to follow OOPs

--- Basic/src/muller.py
+++ Basic/src/muller.py
@@ -75,25 +75,19 @@ class MullerSystem(System):
         pot = (self.VA * np.exp(gaussians))
         return pot.sum()
     
-    @staticmethod
-    def __force(x):
-        if x <= -1.25:
-            return (-8 * (np.pi)**2) * (x + 1.25)
+    def __force(self, x, y):
+        exponent = self.a * (x - self.mean_x)**2 
+        exponent += self.b * (x - self.mean_x) * (y - self.mean_y)  
+        exponent += self.c * (y - self.mean_y)**2
+        V_vector = self.A * np.exp(exponent)
+        F_x = -V_vector * (2 * self.a * (x - mean_x) + self.b * (y - mean_y))
+        F_y = -V_vector * (2 * self.c * (y - mean_y) + self.b * (x - mean_x))
+        F_x = F_x.sum()
+        F_y = F_y.sum()
+        return np.array([F_x, F_y])
         
-        if x > -1.25 and x <= -0.25:
-            return -4 * np.pi * np.cos(2 * np.pi * x)
-            
-        if x >= -0.25 and x <= 0.75:
-            return -6 * np.pi * np.cos(2 * np.pi * x)
                     
-        if x >= 0.75 and x <= 1.75:
-            return -8 * np.pi * np.cos(2 * np.pi * x)
-            
-        if x >= 1.75:
-            return (-16 * (np.pi)**2) * (x - 1.75)
-                    
-    @staticmethod
-    def F(q):
+    def F(self, q):
         """
         Args : 
         q : Vector of size (N, 2) containing N particles on 2D space
@@ -102,23 +96,16 @@ class MullerSystem(System):
         F : Vector of size (N, 2) containing forces on N particles
         """
         
-        A = np.array([-200, -100, -170, 15]).reshape(-1, 1)
-        mean_x = np.array([1, 0, -0.5, -1]).reshape(-1, 1)
-        mean_y = np.array([0, 0.5, 1.5, 1]).reshape(-1, 1)
-        a = np.array([-1, -1, -6.5, 0.7]).reshape(-1, 1)
-        b = np.array([0, 0, 11, 0.6]).reshape(-1, 1)
-        c = np.array([-10, -10, -6.5, 0.7]).reshape(-1, 1)
-        
 
         x = q[:, 0]
         y = q[:, 1]
-        gaussians = a * (x - mean_x)**2
-        gaussians += b * (x - mean_x) * (y - mean_y)
-        gaussians += c * (y - mean_y)**2
-        V_vector = A * np.exp(gaussians)
+        gaussians = self.Va * (x - self.Vmean_x)**2
+        gaussians += self.Vb * (x - self.Vmean_x) * (y - self.Vmean_y)
+        gaussians += self.Vc * (y - self.Vmean_y)**2
+        V_vector = self.VA * np.exp(gaussians)
         
-        F_x = - V_vector * (2 * a * (x - mean_x) + b * (y - mean_y))
-        F_y = - V_vector * (2 * c * (y - mean_y) + b * (x - mean_x))
+        F_x = - V_vector * (2 * self.Va * (x - self.Vmean_x) + self.Vb * (y - self.Vmean_y))
+        F_y = - V_vector * (2 * self.Vc * (y - self.Vmean_y) + self.Vb * (x - self.Vmean_x))
         F_x = F_x.sum(axis = 0).reshape(-1, 1)
         F_y = F_y.sum(axis = 0).reshape(-1, 1)
         return np.hstack((F_x, F_y))

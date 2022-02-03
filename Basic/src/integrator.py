70be9cac38868fd6fb33b0fe48992c5ae102bdb3
Shaunak Badani <shaunak.badani@research.iiit.ac.in>
2022-02-03 18:46:34 +0530

    Fixed tau = 0 problem, need to verify correct runs

--- Basic/src/integrator.py
+++ Basic/src/integrator.py
@@ -265,7 +265,7 @@ class RENSIntegrator(REMDIntegrator):
 
     def step(self, sys, timestep, file_io):
         x = sys.x
-        v = sys.v
+        v = sys.v.copy()
         F = sys.F
         m = sys.m
 
@@ -286,6 +286,7 @@ class RENSIntegrator(REMDIntegrator):
             if not exchange:
                 x_new, v_new = self.x0, self.v0
             else:
+                sys.set_v(v)
                 x_new, v_new = self.exchange_phase_space_vectors(sys)
             return x_new, v_new
     

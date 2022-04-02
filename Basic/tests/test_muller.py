59430a404c799b7f772cfdfe33db00c7729e97fa
Shaunak <shaunak.badani@research.iiit.ac.in>
2022-04-02 19:33:04 -0400

    Added tests for muller system

--- Basic/tests/test_muller.py
+++ Basic/tests/test_muller.py
@@ -11,6 +11,24 @@ class TestMuller(unittest.TestCase):
 
         scalars = pd.read_csv(os.path.join(file_loc, 'scalars.txt'), sep = ' ') 
         self.assertTrue(np.allclose(scalars['TE'], -100.9, atol = 1e-1))
+    
+    def test_muller_nvt(self):
+        os.system('python3 main.py -c tests/Muller/jsons/muller_nvt.json')
+        file_loc = 'tests/Muller/runs/muller_nvt'
+
+        vel = np.loadtxt(os.path.join(file_loc, 'v.txt'))
+
+        p, be = np.histogram(vel[:, 2], density = True)
+        sigma = np.sqrt(2)
+        coords = 0.5 * (be[1:] + be[:-1])
+        expected = np.exp(-coords**2 / (2 * sigma**2)) / np.sqrt(2 * np.pi * sigma**2)
+        self.assertTrue(np.allclose(expected, p, atol = 9.5 * 1e-3))
+
+        p, be = np.histogram(vel[:, 1], density = True)
+        sigma = np.sqrt(2)
+        coords = 0.5 * (be[1:] + be[:-1])
+        expected = np.exp(-coords**2 / (2 * sigma**2)) / np.sqrt(2 * np.pi * sigma**2)
+        self.assertTrue(np.allclose(expected, p, atol = 7.5 * 1e-3))
 
     @classmethod
     def tearDownClass(self):
